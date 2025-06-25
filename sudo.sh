27654ed23a8ac82ce6e37606ea840e1480b7b1a9
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2025-06-25 13:39:39 -0400

    join scripts

--- sudo.sh
+++ sudo.sh
@@ -69,3 +69,16 @@ rm nvim-linux64.tar.gz
 
 echo "[7/9] Ajustando fuso horário"
 ln -sf /usr/share/zoneinfo/America/Manaus /etc/localtime
+
+echo "[8/9] Ajustando fuso horário"
+sudo apt-get update
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
